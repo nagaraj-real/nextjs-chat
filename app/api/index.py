9118b10d186b3027e68551892b6d35d8200a33eb
nagaraj-real <raj.nagaraj1990@gmail.com>
2024-04-01 19:53:14 -0400

    feat: Add re-writes

--- app/api/index.py
+++ app/api/index.py
@@ -9,48 +9,47 @@ from http.server import BaseHTTPRequestHandler
 import os
 from os.path import dirname, abspath, join
 
+from flask import Flask
+
 dir = dirname(abspath(__file__))
-API_KEY = "xf4JNVo6rJOf62gaXRwx8Ppp33AL36KPwk0wQi9Q"
-# Create the embedding model
-embed_model = CohereEmbedding(
-    cohere_api_key=API_KEY,
-    model_name="embed-english-v3.0",
-    input_type="search_query",
-)
+# API_KEY = "xf4JNVo6rJOf62gaXRwx8Ppp33AL36KPwk0wQi9Q"
+# # Create the embedding model
+# embed_model = CohereEmbedding(
+#     cohere_api_key=API_KEY,
+#     model_name="embed-english-v3.0",
+#     input_type="search_query",
+# )
 
-# Create the service context with the cohere model for generation and embedding model
-service_context = ServiceContext.from_defaults(
-    llm=Cohere(api_key=API_KEY, model="command"),
-    embed_model=embed_model
-)
-# Load the data, for this example data needs to be in a test file 
-#data = SimpleDirectoryReader(input_dir="data",input_files=["test.pdf"]).load_data()
-data = SimpleDirectoryReader(input_dir='data',input_files=["test.pdf"]).load_data()
-index = VectorStoreIndex.from_documents(data, service_context=service_context)
+# # Create the service context with the cohere model for generation and embedding model
+# service_context = ServiceContext.from_defaults(
+#     llm=Cohere(api_key=API_KEY, model="command"),
+#     embed_model=embed_model
+# )
+# # Load the data, for this example data needs to be in a test file 
+# #data = SimpleDirectoryReader(input_dir="data",input_files=["test.pdf"]).load_data()
+# data = SimpleDirectoryReader(input_dir='data',input_files=["test.pdf"]).load_data()
+# index = VectorStoreIndex.from_documents(data, service_context=service_context)
 
-# Create a cohere reranker 
-cohere_rerank = CohereRerank(api_key=API_KEY)
+# # Create a cohere reranker 
+# cohere_rerank = CohereRerank(api_key=API_KEY)
 
 
-memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
+# memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
 
-chat_engine = index.as_chat_engine(
-    chat_mode="context",
-    memory=memory,
-    system_prompt=(
-        "You are a chatbot, able to have normal interactions, as well as talk"
-        " about a prospective candidate for hire."
-    ),
-    node_postprocessors=[cohere_rerank]
-)
-response = chat_engine.chat("Hello!")
-print(str(response))
+# chat_engine = index.as_chat_engine(
+#     chat_mode="context",
+#     memory=memory,
+#     system_prompt=(
+#         "You are a chatbot, able to have normal interactions, as well as talk"
+#         " about a prospective candidate for hire."
+#     ),
+#     node_postprocessors=[cohere_rerank]
+# )
+# response = chat_engine.chat("Hello!")
+# print(str(response))
  
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(str(response).encode('utf-8'))
-        return
+app = Flask(__name__)
+
+@app.route("/api/python")
+def hello_world():
+    return "<p>Hello, World!</p>"
--- next.config.js
+++ next.config.js
@@ -9,5 +9,16 @@ module.exports = {
         pathname: '**'
       }
     ]
-  }
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
 }
