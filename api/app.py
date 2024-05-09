79a35a95d1f7d81a379bcc6e6fc2e60dc011f9de
nagaraj <raj.nagaraj1990@gmail.com>
2024-05-09 11:19:49 -0400

    feat: update rag logic

--- api/app.py
+++ api/app.py
@@ -1,40 +1,20 @@
 from pprint import pprint
 from flask import Flask,request
-from langchain_cohere import ChatCohere, CohereEmbeddings, CohereRagRetriever, CohereRerank
+from langchain_cohere import ChatCohere,CohereRagRetriever
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import FAISS
-from langchain.retrievers import ContextualCompressionRetriever
-
 import os
 
 API_KEY = os.environ['COHERE_API_KEY']
 
 def chat(user_query):
-    # Create Cohere's chat model, embeddings and rerank objects.
-    llm = ChatCohere(cohere_api_key=API_KEY)
-    cohere_embeddings = CohereEmbeddings(cohere_api_key=API_KEY)
-    cohere_rerank = CohereRerank(cohere_api_key=API_KEY)
-
-    # Load text files and split into chunks, you can also use data gathered elsewhere in your application.
+    # Use Cohere's RAG retriever in document mode to generate an answer.
+    # Cohere provides exact citations for the sources it used.
+    llm = ChatCohere()
+    rag = CohereRagRetriever(llm=llm, connectors=[])
     loader = PyPDFLoader('data/resum.pdf')
-    pages = loader.load_and_split()
-    # Create a vector store from the documents
-    chroma_index  = FAISS.from_documents(pages, cohere_embeddings)
-
-    # Create Cohere's reranker with the vector DB using Cohere's embeddings as the base retriever.
-    compression_retriever = ContextualCompressionRetriever(
-        base_compressor=cohere_rerank,
-        base_retriever=chroma_index.as_retriever()
-    )
-    compressed_docs = compression_retriever.get_relevant_documents(user_query)
-    # Print the relevant documents from using the embeddings and reranker
-    print(compressed_docs)
-
-    # Create the cohere rag retriever using the chat model
-    rag = CohereRagRetriever(llm=llm)
     docs = rag.get_relevant_documents(
         user_query,
-        documents=compressed_docs,
+        documents=loader.load(),
     )
     answer = docs.pop()
     return answer
