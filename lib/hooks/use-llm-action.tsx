f9ff5f57634952bc4088b869bd69ef6842bc6303
nagaraj <raj.nagaraj1990@gmail.com>
2024-05-09 11:49:39 -0400

    fix: update timeout limit

--- lib/hooks/use-llm-action.tsx
+++ lib/hooks/use-llm-action.tsx
@@ -3,11 +3,7 @@ import { nanoid } from "nanoid";
 
 export function useModelAction() {
     async function submitUserMessage(message: string) {
-        const response = await fetch("https://shortly-heroic-owl.ngrok-free.app/api/chat?question=" + message, {
-            headers: {
-                "ngrok-skip-browser-warning": "yes",
-            }
-        },);
+        const response = await fetch("/api/chat?question=" + message);
         const responseMessage = await response.text();
         return {
             id: nanoid(),
